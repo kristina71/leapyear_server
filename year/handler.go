6f8f030c6231058ee2bbc43074f3e9ead3da1258
kristina71gett <kristina.ezikova@gett.com>
2021-02-22 19:44:18 +0300

    Implement POST handler

--- year/handler.go
+++ year/handler.go
@@ -1,12 +1,14 @@
 package year
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func GetHandler(w http.ResponseWriter, r *http.Request) {
 	strYear, err := strconv.ParseUint(r.URL.Query().Get("year"), 10, 64)
 	if err != nil {
 		log.Println(err)
@@ -16,6 +18,30 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	response := "Year is leap"
 	if !IsLeapYear(strYear) {
+		response = "Year isn't leap"
+	}
+	w.Write([]byte(response))
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	rec := struct {
+		Year uint64
+	}{}
+	err = json.Unmarshal(body, &rec)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	response := "Year is leap"
+	if !IsLeapYear(rec.Year) {
 		response = "Year isn't leap"
 	}
 	w.Write([]byte(response))
