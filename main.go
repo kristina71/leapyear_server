a780ff8e897d08fd3a73f914ad437cf5e63c192b
kristina71gett <kristina.ezikova@gett.com>
2021-02-22 19:47:52 +0300

    Call GET and POST methods

--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"leapyear/year"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/leapYear", year.Handler)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/leapYear", year.GetHandler).Methods(http.MethodGet)
+	r.HandleFunc("/leapYear", year.PostHandler).Methods(http.MethodPost)
+	http.Handle("/", r)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
